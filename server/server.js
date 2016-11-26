7df92e455f744f9ef4686d46ea826bfdd3406c23
David Wilbanks <dwilbank@gmail.com>
2016-11-26 08:45:09 -0500

    add CORS fix to server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,12 @@ app.use(bodyParser.json());
 //     next();
 // })
 
-
+app.use(function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
 
 app.post('/todos', authenticate, (req,res) => {
     var todo = new Todo({
